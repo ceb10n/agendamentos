f849fde1c763e978242b91faff0a9d5dc5a4562f
Rafael Marques <rafaelomarques@gmail.com>
2019-02-24 22:50:11 -0300

    adicionado testes do servico de salas

---
+++ tests/services/test_sala_service.py
@@ -0,0 +1,103 @@
+# -*- coding: utf-8 -*-
+import datetime
+import pytest
+import uuid
+
+from agendamentos.models import db
+from agendamentos.services import SalaService
+from agendamentos import create_app
+
+
+@pytest.fixture(scope='module')
+def agendamentos_app():
+    app = create_app(testing=True)
+    test_app = app.test_client()
+
+    ctx = app.app_context()
+    ctx.push()
+
+    db.create_all()
+
+    yield test_app
+
+    ctx.pop()
+
+
+def test_adicionar_uma_sala_com_sucesso(agendamentos_app):
+    service = SalaService()
+    id = str(uuid.uuid4())
+
+    sala = service.adicionar(
+        id=id,
+        nome='Sala de Teste',
+        codigo='SALA_TESTE')
+
+    assert sala.id == id
+    assert sala.nome == 'Sala de Teste'
+    assert sala.codigo == 'SALA_TESTE'
+
+
+def test_listar_salas(agendamentos_app):
+    service = SalaService()
+    id = str(uuid.uuid4())
+    service.adicionar(id=id, nome='Sala de Teste', codigo='SALA_TESTE')
+
+    salas = service.listar()
+
+    assert len(salas) > 0
+
+
+def test_procurar_sala_por_id(agendamentos_app):
+    service = SalaService()
+    id = str(uuid.uuid4())
+    service.adicionar(id=id, nome='Sala de Teste', codigo='SALA_TESTE')
+
+    sala = service.procurar_por_id(id)
+
+    assert sala.id == id
+    assert sala.nome == 'Sala de Teste'
+    assert sala.codigo == 'SALA_TESTE'
+
+
+def test_editar_sala(agendamentos_app):
+    service = SalaService()
+    id = str(uuid.uuid4())
+    service.adicionar(id=id, nome='Sala de Teste', codigo='SALA_TESTE')
+
+    assert service.editar(id, {
+        'nome': 'Nome Editado',
+        'codigo': 'Código Editado'}) is True
+
+    sala = service.procurar_por_id(id)
+
+    assert sala.id == id
+    assert sala.nome == 'Nome Editado'
+    assert sala.codigo == 'Código Editado'
+
+
+def test_editar_sala_deve_retornar_false_se_a_sala_nao_existir(agendamentos_app): # noqa
+    service = SalaService()
+    id = str(uuid.uuid4())
+
+    assert not service.editar(id, {
+        'nome': 'Nome Editado',
+        'codigo': 'Código Editado'})
+
+
+def test_remover_uma_sala_com_sucesso(agendamentos_app):
+    service = SalaService()
+    id = str(uuid.uuid4())
+
+    service.adicionar(
+        id=id,
+        nome='Sala de Teste',
+        codigo='SALA_TESTE')
+
+    assert service.remover(id) is True
+
+
+def test_remover_uma_sala_que_nao_existe_deve_retornar_falso(agendamentos_app):
+    service = SalaService()
+    id = str(uuid.uuid4())
+
+    assert not service.remover(id)
